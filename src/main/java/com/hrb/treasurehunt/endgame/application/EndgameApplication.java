8608a1966f0fbb8e44250f639c656301cddca436
Purushothaman <rajalekshmi.purushothaman@hrblock.com>
2019-06-08 15:22:07 +0530

    initial commit

---
+++ src/main/java/com/hrb/treasurehunt/endgame/application/EndgameApplication.java
@@ -0,0 +1,13 @@
+package com.hrb.treasurehunt.endgame.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndgameApplication.class, args);
+	}
+
+}
